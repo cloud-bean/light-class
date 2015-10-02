f7cf8903eebfb000b2aeb61f8aea4eddafcf4134
hygkui <fevernova.life@gmail.com>
2015-10-02 19:32:18 +0800

    init the project.

---
+++ app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hygkui on 15/10/2.
+ */
+
+var App = angular.module("lightClass", ['ui.router']);
+
+
+
---
+++ router.js
@@ -0,0 +1,35 @@
+/**
+ * Created by hygkui on 15/10/2.
+ */
+App.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+      // setup an abstract state for the tabs directive
+      .state('/', {
+          url: '/',
+          abstract: true,
+          templateUrl: 'index.html'
+      })
+      .state('msg', {
+        url: '/msg',
+        templateUrl: 'templates/msg.tpl.html'
+      })
+      .state('score', {
+        url: '/score',
+        templateUrl: 'templates/score.tpl.html'
+      })
+      .state('profile', {
+        url: '/me',
+        templateUrl: 'templates/profile.tpl.html'
+      })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/msg');
+
+  });
