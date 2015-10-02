8a650a7600158c0eafb403b052310d6e0ce4efe9
xinnix <xinnix2000@gmail.com>
2015-10-03 00:35:45 +0800

    ADD:gulp,bower

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+
+gulp.task('watch', function () {    // 这里的watch，是自定义的，写成live或者别的也行
+    
+    // app/**/*.*的意思是 app文件夹下的 任何文件夹 的 任何文件
+    gulp.watch(['*'],function() {
+    	gulp.src('./*')
+    	.pipe(connect.reload());
+    })
+});
+
+gulp.task('connect', function () {
+  connect.server({
+    root: './',
+    livereload: true
+  });
+});
+
+
+gulp.task('default', ['connect', 'watch']);
