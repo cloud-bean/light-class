f0cb557ecdec7f2b68077fa5de56b2ca2a710b32
xinnix <xinnix2000@gmail.com>
2015-10-03 08:57:20 +0800

    liveload speed up

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,16 @@ var connect = require('gulp-connect');
 gulp.task('watch', function () {    // 这里的watch，是自定义的，写成live或者别的也行
     
     // app/**/*.*的意思是 app文件夹下的 任何文件夹 的 任何文件
-    gulp.watch(['./**/*.*'],['html','js']);
+    gulp.watch(['./**/*.*'],['reload']);
 });
 
 
-gulp.task('html', function () {
-  gulp.src('./**/*.html')
+gulp.task('reload', function () {
+  gulp.src('./**/*.*')
     .pipe(connect.reload());
 });
 
-gulp.task('js', function () {
-  gulp.src('./**/*.js')
-    .pipe(connect.reload());
-});
+
 
 
 gulp.task('connect', function () {
